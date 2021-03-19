493b1e1b8027c1cfa463115f9e762cf49ec1b4cf
malek14062001 <80289229+malek14062001@users.noreply.github.com>
2021-03-19 11:24:43 +0100

    Create enigmesansfichier.h

---
+++ enigmesansfichier.h
@@ -0,0 +1,14 @@
+#ifndef ENIGMESANSFICHIER_H_
+#define ENIGMESANSFICHIER_H_
+
+
+typedef struct 
+{
+  
+}
+enigme generer() ; ///initialisation énigme: génération aléatoire questions réponse
+void afficherEnigme(enigme e, SDL_Surface * screen) ; /// affich énigme
+
+
+
+#endif
